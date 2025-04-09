e13333e56422438af9da56376e21b82c69b7ec73
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-04-09 20:28:28 -0300

    first commit

---
+++ src/test/java/com/quizmaster/quizmaster_api/QuizmasterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizmaster.quizmaster_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizmasterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
