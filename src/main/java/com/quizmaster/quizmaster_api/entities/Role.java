125742cfee8cb70a0c615c9b09b6f5e14ef1374e
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-04-09 20:55:56 -0300

    feat: cria entidades User e Role com relacionamento ManyToMany

---
+++ src/main/java/com/quizmaster/quizmaster_api/entities/Role.java
@@ -0,0 +1,28 @@
+package com.quizmaster.quizmaster_api.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
